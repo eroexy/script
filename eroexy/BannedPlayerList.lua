4a189e49b80f77e0161c3a7d9d3697ee33a199f5
eroexy <eroexy@icloud.com>
2025-12-21 19:25:10 -0500

    Update BannedPlayerList.lua

--- eroexy/BannedPlayerList.lua
+++ eroexy/BannedPlayerList.lua
@@ -1,11 +1,11 @@
 return {
 	BannedUsers = {
 		-- UserIds
-		[758020288] = true,
-		[123456789] = true,
+		[] = true,
+		[] = true,
 
 		-- Usernames
-		["BadPlayer123"] = true,
-		["AnotherFriend"] = true,
+		[""] = true,
+		[""] = true,
 	}
 }
