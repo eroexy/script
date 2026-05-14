50ee9d79278482b0653f3d50eab778826fd34679
eroexy <eroexyX3D@icloud.com>
2026-05-14 00:44:21 -0400

    Update MoonX.lua

--- MoonX.lua
+++ MoonX.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/1c1598e0ce974776"))()
+getgenv().SCRIPT_KEY = "";
+
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/77250e14c33f8485e35ae8b6a23a39263caa1d0a6241e806d51864f36b7abc4d/download"))()
