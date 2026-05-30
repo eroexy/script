5cd7ad2239d808d85d4412d745151927b9b855f1
eroexy <eroexyX3D@icloud.com>
2026-05-30 05:01:16 -0400

    Update MoonX.lua

--- MoonX.lua
+++ MoonX.lua
@@ -1,3 +1,1 @@
-getgenv().SCRIPT_KEY = "";
-
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/77250e14c33f8485e35ae8b6a23a39263caa1d0a6241e806d51864f36b7abc4d/download"))()
+loadstring(game:HttpGet("https://vss.pandauth.com/virtual/file/dbdf69a16e7b40f7"))()
